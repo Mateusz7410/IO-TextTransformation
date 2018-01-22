2d48b7442850d671618f81a1e8a7b305dd3331f8
Mateusz <Mateusz7410@gmail.com>
2018-01-22 23:14:37 +0100

    Poprawnienie i usunięcie niescisłości.

--- ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ ioprojectarchitecture/src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -17,43 +17,7 @@ import java.awt.event.ActionEvent;
  */
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
-public class TextTransformerApplication extends JFrame{
-    public TextTransformerApplication() {
-        initUI();
-    }
-
-    private void initUI() {
-
-        JButton quitButton = new JButton("Quit");
-
-        quitButton.addActionListener((ActionEvent event) -> {
-            System.exit(0);
-        });
-
-        createLayout(quitButton);
-
-        setTitle("Quit button");
-        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-
-    private void createLayout(JComponent... arg) {
-
-        Container pane = getContentPane();
-        GroupLayout gl = new GroupLayout(pane);
-        pane.setLayout(gl);
-
-        gl.setAutoCreateContainerGaps(true);
-
-        gl.setHorizontalGroup(gl.createSequentialGroup()
-                .addComponent(arg[0])
-        );
-
-        gl.setVerticalGroup(gl.createSequentialGroup()
-                .addComponent(arg[0])
-        );
-    }
+public class TextTransformerApplication{
 
     /**
      * Metoda wywołuje uruchomienie aplikacji.
@@ -65,7 +29,6 @@ public class TextTransformerApplication extends JFrame{
 
         EventQueue.invokeLater(() -> {
             TextTransformerApplication ex = ctx.getBean(TextTransformerApplication.class);
-            ex.setVisible(true);
         });
     }
 }
